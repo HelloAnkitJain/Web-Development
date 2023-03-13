bec3310093a8ece1a764447572c127fef7fe9268
HelloAnkitJain <ankit9.work@gmail.com>
2023-03-13 11:33:10 +0530

    js git project added.

---
+++ CheatSheet/JavaScript/JS_Basics_1/script.js
@@ -0,0 +1,20 @@
+//              do-while
+// i = 1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=10);
+
+//              while
+// let i = 1;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+//              for
+
+// for(let i =1;i<=10;i++){
+//     console.log(i);
+// }
+
+
