0f875e92186eb248cd6ab1c12818cb8a2e975058
HelloAnkitJain <ankit9.work@gmail.com>
2023-03-27 01:15:53 +0530

    project added

---
+++ JavaScript/#Javascript Refresh/Programs for Practice/swap without third.js
@@ -0,0 +1,11 @@
+let a = 10
+let b = 20
+
+
+console.log(`before swapping a = ${a} and b = ${b}`)
+
+a = a+b
+b= a-b
+a= a-b
+
+console.log(`after swapping a = ${a} and b= ${b}`)
---
+++ JavaScript/#Javascript Refresh/Programs for Practice/swaping with third.js
@@ -0,0 +1,12 @@
+let a = 10
+let b = 20
+
+
+console.log(`before swapping a = ${a} and b = ${b}`)
+
+temp = a;
+a = b;
+b = temp;
+
+console.log(`after swapping a = ${a} and b= ${b}`)
+
